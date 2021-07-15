29eeffd5f23d42009c24d51e37d5b354e1f9d26d
Kevin Blair <kblair40@gmail.com>
2021-07-15 13:38:47 -0500

    Add styles to EditTodoForm

--- src/EditTodoForm.js
+++ src/EditTodoForm.js
@@ -1,9 +1,30 @@
 import React, { useContext } from "react";
-import useInputState from "./hooks/useInputState";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 import TextField from "@material-ui/core/TextField";
+
+import useInputState from "./hooks/useInputState";
 import { DispatchContext } from "./contexts/todos.context";
 
+const useStyles = makeStyles({
+  form: {
+    marginLeft: "1rem",
+    width: "50%",
+  },
+  textFieldRoot: {
+    "& .MuiFormLabel-root": {
+      color: "#1F2833",
+    },
+    "& .MuiInputBase-root": {
+      borderBottom: "1px solid #1f2833",
+      "&:after": {
+        borderColor: "#0b0c10",
+      },
+    },
+  },
+});
+
 export default function EditTodoForm({ id, task, toggleEditForm }) {
+  const classes = useStyles();
   const dispatch = useContext(DispatchContext);
   const [value, handleChange, reset] = useInputState(task);
   return (
@@ -14,11 +35,12 @@ export default function EditTodoForm({ id, task, toggleEditForm }) {
         reset();
         toggleEditForm();
       }}
-      style={{ marginLeft: "1rem", width: "50%" }}
+      className={classes.form}
     >
       <TextField
         margin="normal"
         value={value}
+        className={classes.textFieldRoot}
         onChange={handleChange}
         fullWidth
         autoFocus

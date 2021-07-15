4bf769709f1b849f33a52a898f11ae67b324aad1
Kevin Blair <kblair40@gmail.com>
2021-07-15 13:13:50 -0500

    Add AppBar styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import TodoApp from "./TodoApp";
-// import "./App.css";
+
+// COLORS
+// Black - #0B0C10
+// Dark Grey - #1F2833
+// Light Grey - #C5C6C7
+// Light Blue - #66FCF1
+// Teal - #45A29E
 
 function App() {
   return <TodoApp />;
--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,23 +1,41 @@
 import React from "react";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 import { Paper, Typography, AppBar, Toolbar, Grid } from "@material-ui/core";
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
 import { TodosProvider } from "./contexts/todos.context";
 
+const useStyles = makeStyles({
+  paperRoot: {
+    padding: 0,
+    margin: 0,
+    height: "100vh",
+    backgroundColor: "#0B0C10",
+  },
+  appBarRoot: {
+    height: "64px",
+    backgroundColor: "#1F2833",
+    color: "#45A29E",
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-start",
+  },
+  headerText: {
+    fontFamily: "Bebas Neue",
+    fontSize: "3rem",
+    letterSpacing: "4px",
+  },
+});
+
 function TodoApp() {
+  const classes = useStyles();
   return (
-    <Paper
-      style={{
-        padding: 0,
-        margin: 0,
-        height: "100vh",
-        backgroundColor: "#fafafa",
-      }}
-      elevation={0}
-    >
-      <AppBar color="primary" position="static" style={{ height: "64px" }}>
-        <Toolbar>
-          <Typography color="inherit">TODOS WITH HOOKS</Typography>
+    <Paper classes={{ root: classes.paperRoot }} elevation={0}>
+      <AppBar position="static" classes={{ root: classes.appBarRoot }}>
+        <Toolbar className={classes.toolbar}>
+          <Typography className={classes.headerText}>TODO</Typography>
         </Toolbar>
       </AppBar>
       <Grid container justify="center" style={{ marginTop: "1rem" }}>

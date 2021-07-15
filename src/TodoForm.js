9b37cc8ed08d1cbea51ce6e2a08374103ecb6c6f
Kevin Blair <kblair40@gmail.com>
2021-07-15 13:36:04 -0500

    Add styles for TodoForm

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,13 +1,33 @@
 import React, { useContext } from "react";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 import { Paper, TextField } from "@material-ui/core";
 import useInputState from "./hooks/useInputState";
 import { DispatchContext } from "./contexts/todos.context";
 
+const useStyles = makeStyles({
+  paper: {
+    margin: "1rem 0",
+    padding: "0 1rem",
+  },
+  textFieldRoot: {
+    "& .MuiFormLabel-root": {
+      color: "#1F2833",
+    },
+    "& .MuiInputBase-root": {
+      borderBottom: "1px solid #1f2833",
+      "&:after": {
+        borderColor: "#0b0c10",
+      },
+    },
+  },
+});
+
 function TodoForm() {
+  const classes = useStyles();
   const [value, handleChange, reset] = useInputState("");
   const dispatch = useContext(DispatchContext);
   return (
-    <Paper style={{ margin: "1rem 0", padding: "0 1rem" }}>
+    <Paper className={classes.paper}>
       <form
         onSubmit={(e) => {
           e.preventDefault();
@@ -18,6 +38,8 @@ function TodoForm() {
         <TextField
           value={value}
           onChange={handleChange}
+          className={classes.textFieldRoot}
+          // InputProps={{classes: {label: }}}
           margin="normal"
           label="Add new todo"
           fullWidth
